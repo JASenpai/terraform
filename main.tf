03839e9854109496a299703bea4b8764a1100be5
JASenpai <jasenpy@gmail.com>
2019-11-11 13:08:35 -0300

    initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region  =   "us-east-2"
+}
+
+resource "aws_instance" "example"{
+    ami =   "ami-00bf61217e296b409"
+    instance_type   =   "t2.micro"
+    tags    =   {
+        Name = "terraform-example"
+    }
+}
